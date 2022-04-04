e48adf8d886d3bc41ebff29cf27083e04cec9ad7
Sei <seitaku@wethink.com.tw>
2022-04-04 15:53:51 +0800

    clean login

--- app/view/login.py
+++ app/view/login.py
@@ -6,32 +6,12 @@ from flask import Blueprint, jsonify, request, flash, abort, render_template, re
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 from sqlalchemy import or_
-
 from app.utils.JsonUtils import AlchemyEncoder
 
 app_login = Blueprint('app_login', __name__)
 
 import logging
 log = logging.getLogger(__name__)
-
-# @app_login.route("/signUp", methods=['GET','POST'])
-# def signUp():
-#     flag = False
-#     try:
-#         print()
-#         if (request.method == 'POST'):
-#             print(request.get_data())
-
-#             data = json.loads(request.get_data())
-#             account = data['account']
-#             password = data['password']
-#             flag = __registerAccount(account, password)
-#     except Exception as ex:
-#         template = "An exception of type {0} occurred. Arguments:{1!r} \n"
-#         message = template.format(type(ex).__name__, ex.args)
-#         log.error('msg: ',message, traceback.format_exc())
-#         return(f'db連線異常')
-#     return render_template('sign_up.html', boolean = True)
 
 @app_login.route("/signUp", methods=['GET','POST'])
 def signUp():
@@ -92,19 +72,14 @@ def login():
                 # if check_password_hash(user.password, password):
                 if True:
                     flash('Logged in successfully!', category='success')
-                    print('\n\n username: ',user.user_name)
                     session['userInfo'] = {'userId':user.id,'role':1}
 
                     left_menu = leftMenu(user.id)
-                    # session['leftMenu'] = left_menu
                     session['leftMenu'] = json.dumps(left_menu, cls=AlchemyEncoder)
-                    print('\n\n\n\n\:',session.get('leftMenu'))
-                    # return redirect(url_for('app_dashboard.leftMenu'))
                     return redirect(url_for('app_login.home'))
                 else:
                     flash('Incorrect username or password, try again.', category='error')
             else:
-                # flash('Please reLogin', category='relogin')
                 flash('Incorrect username or password, try again.', category='error')
 
             # access_token = create_access_token(identity=account)
@@ -144,39 +119,6 @@ def leftMenu(userId):
         left_menu_list = MLeftMenu.query.filter( MLeftMenu.id.in_( left_menu_id ) ).all()
     
     return left_menu_list
-
-
-# @app_login.route("/login", methods=['GET','POST'])
-# def login():
-#     try:
-#         if (request.method == 'POST'):
-#             print(request.get_data())
-#             data = json.loads(request.get_data())
-
-#             account = data['account']
-#             password = data['password']
-#             isSuccess = __verifyPassword(account, password)
-#             print(isSuccess)
-#             if not isSuccess:
-#                 return jsonify({'code':999, 'msg':'登入失敗'})
-
-#             access_token = create_access_token(identity=account)
-#             # return jsonify(access_token=access_token)
-#             print('ss')
-#             return render_template('qprd.html', tables={'code':0, 'msg':''})
-
-#             # return jsonify({'code':0, 'msg':''})
-        
-#     except Exception as e:
-#         # log
-#         abort(404)
-    
-#     userinfo = {
-#         'username': 'xx',
-#         'personalname': 'yy'
-#     }
-    
-#     return render_template('login.html', text='test')
 
 @app_login.route("/home")
 def home():
