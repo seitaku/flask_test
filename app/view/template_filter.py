43b86e9d1192356507ae13f9140d4660d16aa5c4
Sei <seitaku@wethink.com.tw>
2022-04-04 15:43:22 +0800

    left menu css

--- app/view/template_filter.py
+++ app/view/template_filter.py
@@ -6,3 +6,22 @@ app_template_filter = Blueprint('app_template_filter', __name__)
 @app_template_filter.app_template_filter(name="toJson")
 def template_toJson(json_str):
     return json.loads(json_str)
+
+@app_template_filter.app_template_filter(name="getParent")
+def get_parent(api, list):
+    for d in list:
+        if d['path'] == str(api):
+            return d['parent']
+    return None
+
+@app_template_filter.app_template_filter(name="css_")
+def css(self, *p):
+    
+    if ('show' == self):
+        if ( p[0] == p[1] ):
+            return self
+    
+    if ('active' == self):
+        if ( p[0] == p[1] ):
+            return self
+    return ''
