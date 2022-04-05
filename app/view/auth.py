cdaec15c5a51b07055827ff0bc713fe732e00a28
Sei <seitaku@wethink.com.tw>
2022-04-06 01:37:53 +0800

    auth test

--- app/view/auth.py
+++ app/view/auth.py
@@ -25,25 +25,45 @@ def home():
         user_name = request.args.get('userName')
         auth = request.args.get('auth')
         status = request.args.get('status')
-        if status is None :
-            status = 0
-        print(user_name,auth,status)
+        condition = {}
+        condition['user_name']=user_name
+        condition['auth']=auth
+        condition['status']=status
+
+        print('\n\n:',condition)
+
         
-
-
-
         userId= session['userInfo'].get('userId')
         
-        subq = db.session.query(UUserLevel.auth).filter_by(user_id = userId).subquery()
+        # subq = db.session.query(UUserLevel.auth).filter_by(user_id = userId).subquery()
         
         # auth 1-10 高-低
-        mainq = db.session.query(UUser.id, UUser.user_name, UUser.email, UUser.status, UUserLevel.create_by, UUserLevel.auth
-        ).outerjoin(UUserLevel, UUserLevel.user_id == UUser.id 
-        ).filter( and_( 
-        (or_( UUserLevel.auth > subq , UUserLevel.auth == None) )
-        , UUser.status == status ) )
-        print('mainq:',mainq)
-        user_list = mainq.all()
+        # mainq = db.session.query(UUser.id, UUser.user_name, UUser.email, UUser.status, UUserLevel.create_by, UUserLevel.auth
+        # ).outerjoin(UUserLevel, UUserLevel.user_id == UUser.id 
+        # ).filter( and_( 
+        # (or_( UUserLevel.auth > subq , UUserLevel.auth == None) )
+        # , UUser.status == status ) )
+        # user_list = sql.all()
+        sql = f"""
+        select u.id,u.email,u.user_name,u.status, ul.auth,ul.create_by  
+        from u_user u left join u_user_level ul on u.id = ul.user_id
+        where (ul.auth> (
+        SELECT u.auth FROm u_user_level u where id={userId}) or ul.auth is null)
+        """
+
+        if user_name is not None :
+            sql += f' and u.user_name = \'{user_name}\''
+
+        if auth is not None :
+            sql += f' and u.auth = \'{auth}\''
+        print('status:',status)
+        if status is not None and len(status) > 0:
+            sql += f' and u.status = {status}'
+       
+        sql +=';'
+
+        print('sql:',sql)
+        user_list = db.engine.execute(sql).fetchall()
         # print('user_list:',user_list)
         for user in user_list:
             print(user)
@@ -55,8 +75,8 @@ def home():
         abort(404)
     # End login post handler
 
-
-    return render_template('auth.html', user_list=user_list,status = status)
+    print('\n\nstatus:',condition['status'])
+    return render_template('auth.html', user_list=user_list, condition=condition)
 
 
 @app_auth.route("/update", methods=['GET','POST'])
